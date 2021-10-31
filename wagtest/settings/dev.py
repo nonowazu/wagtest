dc8c0f30d944b59ce75e30a2fdec25fde93a361c
Oowazu Nonowazu <oowazu.nonowazu@gmail.com>
2021-10-31 12:03:11 -0500

    Initial commit

---
+++ wagtest/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure--ug#6ysep7q$$mn%!)38d1y8myerxbyq&u%dkqz43cg&d!@xwi'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
